a715e9debf08fea3b068d374766d5ae89f496fe5
Richard Scheglmann <r.scheglmann@msush.de>
2020-07-23 11:08:01 +0200

    Pyhtonscript hinzugefügt, welches einen Server erstellt zum lokalen Entwickeln.

---
+++ StartServer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import http.server
+import os
+
+os.chdir("./")
+port = 8081
+print("Running on port %d" % port)
+
+http.server.SimpleHTTPRequestHandler.extensions_map[".wasm"] = "application/wasm"
+
+httpd = http.server.HTTPServer(
+    ("localhost", port), http.server.SimpleHTTPRequestHandler
+)
+
+print(
+    'Mapping ".wasm" to "%s"'
+    % http.server.SimpleHTTPRequestHandler.extensions_map[".wasm"]
+)
+httpd.serve_forever()
