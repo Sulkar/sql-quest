5d9e56ba914825908994066475a3376af2b88e13
Richard Scheglmann <r.scheglmann@msush.de>
2021-03-08 21:27:14 +0100

    btnAdd hinzugefügt, um mehrere inputFields bei einer Auswahl hinzufügen zu können.

--- js/testcode.js
+++ js/testcode.js
@@ -1,52 +1,75 @@
 $(document).ready(function () {
     var id = 0;
     var currentSelectedElement = 0;
+    var leerzeichen = "<span class='codeElement leerzeichen'> </span>";
 
-    // Buttons
-    $('#btnSelect').click(function () {
-        var elementSELECT = "<span id='select_" + id + "' class='codeElement' data-type=''>SELECT <span id='select_child_" + id + "' class='codeElement child' data-type='myInput'>___ </span></span>";
+    // Button: SELECT * FROM *
+    $('.btnSelectFrom').click(function () {
+        var elementSELECT_FROM = "<span id='codeElement_" + id + "' class='codeWrapper parent'>";
+        elementSELECT_FROM += "<span id='codeElement_" + (id + 1) + "' class='codeElement child'>SELECT" + leerzeichen;
+        elementSELECT_FROM += "<span id='codeElement_" + (id + 2) + "' class='codeElement child inputField root dbField'>___</span></span>";
+        elementSELECT_FROM += leerzeichen + "<span id='codeElement_" + (id + 3) + "' class='codeElement child'>FROM" + leerzeichen;
+        elementSELECT_FROM += "<span id='codeElement_" + (id + 4) + "' class='codeElement child inputField root dbTable'>___</span></span>";
+        elementSELECT_FROM += "</span></span>";
         if (currentSelectedElement == 0) {
-            $('#targetArea').append(elementSELECT);
-        } else {
-            if ($("#" + currentSelectedElement).data("type") == "myInput") {
-                $("#" + currentSelectedElement).html(elementSELECT);
-            }
-
+            $('#targetArea').append(elementSELECT_FROM);
         }
-        id++;
-    });
-    $('#btnFrom').click(function () {
-        $('#targetArea').append("<span id='from_" + id + "' class='codeElement' data-id='" + id + "'>FROM <span id='from_child_" + id + "' class='codeElement child' data-id='" + id + "'>___ </span></span>");
-        id++;
+        id = id + 5;
     });
 
-    $('#btnDelete').click(function () {
-        if (!$("#" + currentSelectedElement).hasClass("child")) {
-            $("#" + currentSelectedElement).remove();
+    //Button: Add Element
+    $('.btnAdd').click(function () {
+        var tempSelection = "#" + currentSelectedElement;
+        if ($(tempSelection).hasClass("inputField")) {
+            $(tempSelection).parent().append("<span id='codeElement_" + id + "' class='codeElement child inputField extended'>,___</span>");
+            removeSelection();
+            $("#codeElement_" + id).addClass("active");
+            currentSelectedElement = "codeElement_" + id;
+            id++;
+        }
+    });
+
+    // Button: Delete Element
+    $('.btnDelete').click(function () {
+        var tempSelection = "#" + currentSelectedElement;
+        if (!$(tempSelection).hasClass("child") || $(tempSelection).hasClass("extended")) {
+            $(tempSelection).remove();
             currentSelectedElement = 0;
+        } else if ($(tempSelection).hasClass("inputField") && $(tempSelection).hasClass("root")) {
+            $(tempSelection).html("___");
         } else {
-            $("#" + currentSelectedElement).parent().addClass("active");
-            currentSelectedElement = $("#" + currentSelectedElement).parent().attr('id');
+            $(tempSelection).parent().addClass("active");
+            currentSelectedElement = $(tempSelection).parent().attr('id');
         }
     });
 
+    // on Click Element
     $('body').on('click', 'span', function (event) {
         event.stopPropagation();
-        $(".codeElement").removeClass("active");
+        removeSelection();
         $(this).addClass("active");
         currentSelectedElement = $(this).attr('id');
         $("#debug").html("Selected: " + currentSelectedElement);
     });
+
+    // on Click TargetArea - deselct
     $('body').on('click', '#targetArea', function (event) {
         event.stopPropagation();
         removeSelection();
     });
 
-    $('#tables').on('change', function () {
-        $("#" + currentSelectedElement).html(this.value + " ");
+    // Select: add dbField, dbTable
+    $('.selectElement').on('change', function () {
+        var tempSelection = "#" + currentSelectedElement;
+        if ($(tempSelection).hasClass("extended")) {
+            $(tempSelection).html("," + this.value);
+        } else {
+            $(tempSelection).html(this.value);
+        }
         removeSelection();
     });
 
+    //function: remove Selectioon from all Elements
     function removeSelection() {
         $(".codeElement").removeClass("active");
         currentSelectedElement = 0;
